9e1473b3c281d1dc3111e30c4887394bef6a14d7
Andres marin <miniyluna02@gmail.com>
2026-05-18 03:15:13 -0500

    caracteristica menu hamburguesa, cerrar si hay click fuera del menu o el boton

--- archivo.js
+++ archivo.js
@@ -3,4 +3,10 @@ const boton = document.querySelector(".hamburguesa");
 const menu = document.querySelector(".menu");
 boton.addEventListener("click", function () {
     menu.classList.toggle("activo");
-    });
+});
+document.addEventListener("click", function(evento) {
+    if(!menu.contains(evento.target) && !boton.contains(evento.target)) {
+        menu.classList.remove("activo");
+    }
+});
+   
