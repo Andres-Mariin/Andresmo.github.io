18fe74c2475870e835d326ee9e2b38d00a3c2763
Andres marin <miniyluna02@gmail.com>
2026-05-18 03:47:28 -0500

    añado historia de cuadrado interactivo

---
+++ historia3_cuadrado.js
@@ -0,0 +1,22 @@
+
+let botonColor = document.querySelector(".boton1");
+let botonContador = document.querySelector(".boton2");
+let color = document.querySelector(".cuadroColoreado");
+let texto = document.querySelector("p");
+let contador = 0;
+botonColor.addEventListener("click", function () {
+    color.classList.toggle("black");
+    contador = contador + 1;
+});
+botonContador.addEventListener("click", function () {
+    if(color.classList.contains("black")){
+        texto.style.color = "white";
+        texto.textContent = contador + " Clicks!!";
+    }else{
+        texto.style.color = "black";
+        texto.textContent = contador + " Clicks!!";
+    }
+    if(contador === 1) {
+        texto.textContent = contador + " Click!!";
+    }
+}); 
