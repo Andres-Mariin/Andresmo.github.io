87fcecc47246e8363f2de87296396ca2e26b0450
Andres marin <miniyluna02@gmail.com>
2026-05-19 17:11:52 -0500

    optimizacion codigo cuadro interactivo

--- historia3_cuadrado.js
+++ historia3_cuadrado.js
@@ -4,12 +4,15 @@ let botonContador = document.querySelector(".boton2");
 let color = document.querySelector(".cuadroColoreado");
 let texto = document.querySelector(".texto-interactivo");
 let contador = 0;
-botonColor.addEventListener("click", function () {
-    color.classList.toggle("black");
+botonColor.addEventListener("click", function() {
     contador = contador + 1;
+    color.classList.toggle("black");
 });
-botonContador.addEventListener("click", function () {
-    if(color.classList.contains("black")){
+botonContador.addEventListener("click", function() {
+    if(texto.textContent !== "") {
+        texto.textContent = "";
+    }else{
+        if(color.classList.contains("black")) {
         texto.style.color = "white";
         texto.textContent = contador + " Clicks!!";
     }else{
@@ -17,6 +20,12 @@ botonContador.addEventListener("click", function () {
         texto.textContent = contador + " Clicks!!";
     }
     if(contador === 1) {
-        texto.textContent = contador + " Click!!";
+        texto.textContent = contador + " click!!"
     }
-}); 
+    }
+});
+document.addEventListener("click", function(event) {
+    if(!botonContador.contains(event.target) && (botonColor.contains(event.target))) {
+        texto.textContent = "";
+    }
+});
