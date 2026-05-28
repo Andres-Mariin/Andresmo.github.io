04b66a99d4504b36318c77296dff1510e56f1047
Andres marin <miniyluna02@gmail.com>
2026-05-28 13:30:55 -0500

    creacion overlay menu hamburguesa

--- archivo.js
+++ archivo.js
@@ -1,12 +1,15 @@
 
 const boton = document.querySelector(".hamburguesa");
 const menu = document.querySelector(".menu");
+let overlay = document.querySelector(".overlay");
 boton.addEventListener("click", function () {
+    overlay.classList.toggle("activo");
     menu.classList.toggle("activo");
 });
 document.addEventListener("click", function(evento) {
     if(!menu.contains(evento.target) && !boton.contains(evento.target)) {
         menu.classList.remove("activo");
+        overlay.classList.remove("activo");
     }
 });
    
