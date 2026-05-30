516ac3d99f1ca063807030423d256fe836be8614
Andres marin <miniyluna02@gmail.com>
2026-05-29 22:43:24 -0500

    bloqueo de scroll al desplegar menu hamburguesa

--- archivo.js
+++ archivo.js
@@ -2,14 +2,17 @@
 const boton = document.querySelector(".hamburguesa");
 const menu = document.querySelector(".menu");
 let overlay = document.querySelector(".overlay");
+let body = document.querySelector("body");
 boton.addEventListener("click", function () {
     overlay.classList.toggle("activo");
     menu.classList.toggle("activo");
+    body.classList.toggle("bloquearScroll");
 });
 document.addEventListener("click", function(evento) {
     if(!menu.contains(evento.target) && !boton.contains(evento.target)) {
         menu.classList.remove("activo");
         overlay.classList.remove("activo");
+        body.classList.toggle("bloquearScroll");
     }
 });
    
