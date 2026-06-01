2c88146d82dd32daa993a4ab8b555d896b143753
Andres marin <miniyluna02@gmail.com>
2026-06-01 18:37:09 -0500

    caracteristica selector de fotos de galeria

--- archivo.js
+++ archivo.js
@@ -1,6 +1,6 @@
 
-const boton = document.querySelector(".hamburguesa");
-const menu = document.querySelector(".menu");
+let boton = document.querySelector(".hamburguesa");
+let menu = document.querySelector(".menu");
 let overlay = document.querySelector(".overlay");
 let body = document.querySelector("body");
 boton.addEventListener("click", function () {
@@ -15,4 +15,15 @@ document.addEventListener("click", function(evento) {
         body.classList.remove("bloquearScroll");
     }
 });
-   
+
+let smallImage = document.querySelectorAll(".fotoPequena");
+let lightBox = document.querySelector(".lightBox");
+smallImage.forEach(function(smallImage) {
+    smallImage.addEventListener("click", function() {
+        lightBox.src = smallImage.src;
+        lightBox.classList.toggle("fotoFlotanteActiva");
+    });
+});
+lightBox.addEventListener("click", function() {
+    lightBox.classList.remove("fotoFlotanteActiva");
+})
